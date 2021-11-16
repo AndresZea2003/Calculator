25d0f58f6a49a7bb4fe4c3014ee6786c3a606ea5
AndresZea2003 <afzea2003@gmail.com>
2021-11-16 14:58:40 -0500

    feat: Base template creation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,105 @@
 <template>
-  <h1>Calculator</h1>
+  <div class="bg-red-100 h-screen">
+    
+    <div class="bg-red-100 grid grid-cols-2">
+
+      <div>
+        <h1 class="text-6xl text-red-400 text-center">
+        Calculator
+       </h1>
+        <div><input class="focus:ring-2 focus:ring-blue-600 ...">
+          <button class="border-2 focus:ring-blue-600 hover:border-black shadow-inner">
+          Buscar
+          </button> <br>
+        
+
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">1</button>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">2</button>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">3</button><br>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">4</button>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">5</button>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">6</button><br>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">7</button>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">8</button>
+          <button class="border-2 focus:ring-blue-600 hover:border-gray-500 ">9</button>
+        </div>
+    
+        
+      </div>
+      
+        <div>
+        
+         <h1 class="text-6xl text-red-400 text-center">
+         Historial
+        </h1>
+      
+        </div>
+      
+
+      
+    </div>
+    
+  
+      
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  </div>
 
   
-  <icon width="100px"/>
+ <icon width="100px"/>
+  
+  
+
+
+
+
+
+<div class="bg-gray-600 grid grid-cols-2">
+  <div class="bg-blue-600">1</div>
+  <div class="bg-red-600">2</div>
+ </div>
+
+
+
+
+
+
+
+
+
+
 </template>
 
 <script>
