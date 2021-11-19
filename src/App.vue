747262b13d9afdc2982a086989f485bb39e5ba55
AndresZea2003 <afzea2003@gmail.com>
2021-11-18 19:24:17 -0500

    feat: Functional buttons added

--- src/App.vue
+++ src/App.vue
@@ -1,150 +1,62 @@
 <template>
   <div class="bg-green-500 h-screen">
     
-    <div class="bg-gray-800 grid grid-cols-2 h-auto border-8 border-black ">
-
-      <div class="grid grid-cols-3 border-dashed">
-        <h1 class="text-5xl text-white text-left ml-8 mt-8 font2">
-         Calculator
-        </h1>
-        <h1 class="text-3xl text-gray-500 text-left font1 ml-59 mt-0">
-         By Andres Zea
-        </h1>
-
-        
+    <AppHeader/>
     
-        
-      </div>
-       
-      <div class="grid grid-cols-3">
-        
-          <button target="blank"  class="my-4 mx-10 facebook1 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-            <a href="https://www.facebook.com/andresfelipe.zea">
-             Facebook
-            </a>
-          </button>
-         <button class="my-4 mx-10 github1 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-          <a href="https://github.com/AndresZea2003">Github</a>  
-          </button>
-          <button class="my-4 mx-10 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-           <a href="https://www.youtube.com/channel/UCnVVCn92d-329QXd8WRp0VA">Youtube</a>  
-          </button>
-
-          
-
-        
-        
-          
-      
-      </div>
-      
-    </div>
+  
+  
     <div class="bg-gray-600 grid grid-cols-2">
         <div class="bg-gray-400 mx-20 my-11 border-8 border-solid border-black">
 
           
           <div class="grid grid-cols-2 bg-gray-300">
-            <input class="focus:ring-2 focus:ring-blue-600 object-right py-10 px-20 ml-20">
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-red-500 text-white ml-24">
+            <h1 class="text-3xl bg-gray-400 font1 text-white my-4 ml-4 py-3 px-2">{{text}}</h1>
+            <button @click='cambiaText0()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-red-500 text-white ml-24">
              X
             </button>
+          
           </div>
           <div class="flex grid grid-cols-4 gap-x-0 gap-y-5 justify-items-center">
 
             
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">1</button>
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">2</button>
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">3</button>
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">÷</button>
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">4</button>
+
             
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">5</button>
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">6</button>
-             <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">×</button>
+             <button @click='cambiaText()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">1</button>
+             <button @click='cambiaText1()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">2</button>
+             <button @click='cambiaText2()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">3</button>
+             <button @click='cambiaText3()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">÷</button>
+             <button @click='cambiaText4()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">4</button>
+            
+             <button @click='cambiaText5()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">5</button>
+             <button @click='cambiaText6()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">6</button>
+             <button @click='cambiaText7()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">×</button>
            
 
             
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">7</button>
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">8</button>
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">9</button>
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">-</button>
+            <button @click='cambiaText8()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">7</button>
+            <button @click='cambiaText9()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">8</button>
+            <button @click='cambiaText10()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">9</button>
+            <button @click='cambiaText11()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">-</button>
 
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">C</button>
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">0</button>
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">.</button>
-            <button class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">=</button>
+            <button @click='cambiaText12()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">C</button>
+            <button @click='cambiaText13()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-gray-500 text-white">0</button>
+            <button @click='cambiaText14()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">.</button>
+            <button @click='cambiaText15()' class="border-2 focus:ring-blue-600 hover:border-gray-500 text-6xl rounded-full h-24 w-24 flex items-center justify-center bg-green-700 text-white">=</button>
             
 
           </div>
         </div>
+
         <div class="bg-red-600">
           <h1 class="text-6xl text-red-400 text-right py-8 px-8">Historial</h1>
         </div>
-     </div>
+    </div>
     
-      
-    
-  <svg class="animate-bounce w-6 h-6 ...">
-  <svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
-</svg>
 
- 
-  <button class="my-4 mx-10 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
-           Youtube
-          </button>
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   
   </div>
 
   
- <icon width="100px"/>
- <icon width="100px"/>
- <MasIcon>safafaf</MasIcon>/> agsagaegegaegae
-  <mas-icon/>
-  
-
-
-
-
-
-<div class="bg-gray-600 grid grid-cols-2">
-  <div class="bg-blue-600">1</div>
-  <div class="bg-red-600">2</div>
- </div>
-
-
-
-
-
-
 
 
 
@@ -157,7 +69,67 @@
 import Facebool from './components/assets/Facebool.vue';
 import Icon from './components/assets/Icon.vue';
 import MasIcon from './components/assets/MasIcon.vue';
+import AppHeader from './components/AppHeader.vue'
+import { ref } from 'vue';
 export default {
-  components: {Icon, MasIcon, Facebool}
+  components: {Icon, MasIcon, Facebool, AppHeader},
+
+  setup() {
+    const text = ref('Escribe una operacion :D')
+    const cambiaText = () => {
+      text.value = text.value+'1'
+    }
+    const cambiaText0 = () => {
+      text.value = ''
+    }
+    const cambiaText1 = () => {
+      text.value = text.value+'2'
+    }
+    const cambiaText2 = () => {
+      text.value = text.value+'3'
+    }
+    const cambiaText3 = () => {
+      text.value = text.value+'÷'
+    }
+    const cambiaText4 = () => {
+      text.value = text.value+'4'
+    }
+    const cambiaText5 = () => {
+      text.value = text.value+'5'
+    }
+    const cambiaText6 = () => {
+      text.value = text.value+'6'
+    }
+    const cambiaText7 = () => {
+      text.value = text.value+'×'
+    }
+    const cambiaText8 = () => {
+      text.value = text.value+'7'
+    }
+    const cambiaText9 = () => {
+      text.value = text.value+'8'
+    }
+    const cambiaText10 = () => {
+      text.value = text.value+'9'
+    }
+    const cambiaText11 = () => {
+      text.value = text.value+'-'
+    }
+    const cambiaText12 = () => {
+      text.value = text.value+'+'
+    }
+    const cambiaText13 = () => {
+      text.value = text.value+'0'
+    }
+    const cambiaText14 = () => {
+      text.value = text.value+'.'
+    }
+    const cambiaText15 = () => {
+      text.value = text.value+'='
+    }
+   
+    return {text, cambiaText, cambiaText0, cambiaText1, cambiaText2, cambiaText3, cambiaText4, cambiaText5, cambiaText6, cambiaText7, cambiaText8, cambiaText9, cambiaText10, cambiaText11, cambiaText12, cambiaText13, cambiaText14, cambiaText15}
+  }
 }
 </script>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    
+<div class="bg-gray-800 grid grid-cols-2 h-auto border-8 border-black ">
+
+        <div class="grid grid-cols-3 border-dashed">
+            <h1 class="text-5xl text-white text-left ml-8 mt-8 font2">
+            Calculator
+             </h1>
+            <h1 class="text-3xl text-gray-500 text-left font1 ml-59 mt-0">
+            By Andres Zea
+            </h1>
+
+        
+    
+        
+        </div>
+       
+        <div class="grid grid-cols-3">
+        
+          <button target="blank"  class="my-4 mx-10 facebook1 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+            <a href="https://www.facebook.com/andresfelipe.zea">
+             Facebook
+            </a>
+          </button>
+         <button class="my-4 mx-10 github1 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+          <a href="https://github.com/AndresZea2003">Github</a>  
+          </button>
+          <button class="my-4 mx-10 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+           <a href="https://www.youtube.com/channel/UCnVVCn92d-329QXd8WRp0VA">Youtube</a>  
+          </button>
+
+          
+
+        
+        
+          
+      
+        </div>
+</div>
+      </template>
